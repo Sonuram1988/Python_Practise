bc544e063564434e004e2119366aa9dd7d944c88
shiva <shivkartik1@gmail.com>
2022-12-13 12:23:56 +0530

    fdsdf

--- pr_011_05.py
+++ pr_011_05.py
@@ -1,4 +1,5 @@
 # complex numbers
+#(a+bi)(c+di) = (ac−bd) + (ad+bc)i
 
 class Complex:
     def __init__(self,r,i):
@@ -8,10 +9,17 @@ class Complex:
     def __add__(self,c):
         return Complex(c.real+self.real,c.imaginary+self.imaginary)
     
+    def __mul__(self,c):
+        mulReal=c.real*self.real-c.imaginary*self.imaginary
+        mulimaginary=c.imaginary*self.real+c.real*self.imaginary
+        return Complex(mulReal,mulimaginary)
+    
     def __str__(self):
         return f"{self.real}+{self.imaginary}i"
 
-c1=Complex(2,4)
-c2=Complex(1,2)
+c1=Complex(3,2)
+c2=Complex(1,7)
 sum=c1+c2
 print(sum)
+mul=c1*c2
+print(mul)
