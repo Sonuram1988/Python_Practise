d020ec5c290d37eb6773e0092c15be80a8dc0dbd
shiva <shivkartik1@gmail.com>
2022-12-18 10:24:12 +0530

    fsdwfw

---
+++ join.py
@@ -0,0 +1,5 @@
+l1=["Mobile","Laptop","Scanner","Printer","Ipad"]
+new_str="\n".join(l1)
+print(new_str)
+print(type(new_str))
+
---
+++ lambda.py
@@ -0,0 +1,13 @@
+# def func(x):
+#     return x+5
+# print(func(600))
+
+
+# func=lambda x:x+5
+# x=500
+# print(func(x))
+
+square=lambda a:a*a
+print(square(4))
+sum=lambda a,b:a+b
+print(sum(10,25))
