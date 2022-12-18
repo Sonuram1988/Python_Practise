88c14f421b5b1fed66925d52c606c146e4c1883c
shiva <shivkartik1@gmail.com>
2022-12-18 18:03:26 +0530

    ffw

---
+++ format.py
@@ -0,0 +1,11 @@
+name="Sonu Ram"
+profile="Python Developer"
+# a=f"My name is {name}"
+a="my name is {}.He is {}".format(name,profile)
+print(a)
+
+firstName="Sonu"
+# age=34
+# cd="coding"
+str="Hello,Hi this is {}.My age is {}.I love to {}".format(firstName,34,"coding")
+print(str)
---
+++ map.py
@@ -0,0 +1,13 @@
+# l1=[2,3,4]
+# def square(num):
+#     return num*num
+
+# l2=map(square,l1)
+# print(list(l2))
+
+li=[3,4,5]
+def cube(num):
+    return num*num*num
+
+l1=map(cube,li)
+print(list(l1))
